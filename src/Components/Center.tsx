c4d1e9548699119779fa2cbb5a6c5670e15cf72e
Aleksandr <a13x.student@yandex.ru>
2021-03-30 15:06:42 +0300

    center and top done

--- src/Components/Center.tsx
+++ src/Components/Center.tsx
@@ -1,7 +1,18 @@
 import '../CSS/Center.css'
+import littleIce1 from '../Assets/img/ice copy 6.png'
+import littleIce2 from '../Assets/img/ice copy 4.png'
+import middleIce from '../Assets/img/ice copy 5.png'
+import bigIce from '../Assets/img/ice copy 3.png'
 
 export const Center = () => {
     return(
-        <div className="second"></div>
+        <div className="second">
+            <img src={littleIce1} className="second__ice1 ice" alt=""/>
+            <img src={littleIce2} className="second__ice2 ice" alt=""/>
+            <img src={middleIce} className="second__ice3 ice" alt=""/>
+            <img src={bigIce} className="second__ice4 ice" alt=""/>
+            <p className="second__header">Основа терапии —
+            патогенез СД2</p>
+        </div>
     )
 }
--- src/Components/Top.tsx
+++ src/Components/Top.tsx
@@ -2,6 +2,24 @@ import '../CSS/Top.css'
 
 export const Top = () => {
     return (
-        <div className="first"></div>
+        <div className="top">
+            <div className="top__circle1 circle1"></div>
+            <div className="top__circle-animation1 circle1"></div>
+            <div className="top__circle-animation2 circle1"></div>
+
+            <div className="top__circle2 circle2"></div>
+            <div className="top__circle-animation1 circle2"></div>
+            <div className="top__circle-animation2 circle2"></div>
+
+            <div className="top__circle3 circle3"></div>
+            <div className="top__circle-animation1 circle3"></div>
+            <div className="top__circle-animation2 circle3"></div>
+
+            <div className="top__circle3 circle4"></div>
+            <div className="top__circle-animation1 circle4"></div>
+            <div className="top__circle-animation2 circle4"></div>
+            <p className="top__header">Всегда ли цели терапии СД2
+            на поверхности?</p>
+        </div>
     )
 }
